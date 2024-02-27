deb55542510d14064e59c573525060e53d491e16
bcwongaa <bcwongaa@gmail.com>
2024-02-27 21:47:56 +0800

    Have a working hardhat test with compilable contract

--- test/AirdropToken.ts
+++ test/AirdropToken.ts
@@ -3,28 +3,22 @@ import {
   loadFixture,
 } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
-import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { BigNumberish } from 'ethers';
+import { expect } from 'chai';
 
 describe('AirdropToken', function () {
-  async function deployTokenContract() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    // const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  it('Should deploy', async function () {
     const name: string = 'Token1';
     const symbol: string = 'TT1';
-    const cap: BigNumberish = 1e24; // 1million;
+    const cap: BigNumberish = ethers.parseEther('1000000'); // 1 million;
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // const [owner, otherAccount] = await ethers.getSigners();
 
     const AirdropToken = await ethers.getContractFactory('AirdropToken');
     const airdropToken = await AirdropToken.deploy(name, symbol, cap);
 
-    return { airdropToken, name, symbol, cap. owner, otherAccount };
-  }
+    expect(await airdropToken.cap()).to.equal(cap);
+  });
 });
-
